684a7d2a7544b15f04807751b13af5c81bfd09f8
Bader Alrifai <bedrrifai1@gmail.com>
2022-09-30 00:34:51 -0700

    Test Commit

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-const { initializeAppCheck, ReCaptchaV3Provider } = require("firebase/app-check");
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -35,7 +35,7 @@ const appCheck = initializeAppCheck(app, {
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Welcome!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>

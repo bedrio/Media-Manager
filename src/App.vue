489827014cd95db36adb5d74a1d42e345042641f
Bader Alrifai <bedrrifai1@gmail.com>
2022-10-03 23:03:59 -0700

    Added the Quasar plugin as a component library

--- src/App.vue
+++ src/App.vue
@@ -36,7 +36,16 @@ const appCheck = initializeAppCheck(app, {
 
     <div class="wrapper">
       <HelloWorld msg="Welcome!" />
-
+      <div class="q-pa-md q-gutter-sm">
+        <q-btn color="white" text-color="black" label="Standard" />
+        <q-btn color="primary" label="Primary" />
+        <q-btn color="secondary" label="Secondary" />
+        <q-btn color="amber" glossy label="Amber" />
+        <q-btn color="brown-5" label="Brown 5" />
+        <q-btn color="deep-orange" glossy label="Deep Orange" />
+        <q-btn color="purple" label="Purple" />
+        <q-btn color="black" label="Black" />
+      </div>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+import { Quasar } from 'quasar'
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
 
 const app = createApp(App)
+app.use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+})
 
 app.use(router)
 

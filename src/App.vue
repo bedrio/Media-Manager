9b3b7dacafef558888ef1ddc3c71bd66c2609961
Bader Alrifai <bedrrifai1@gmail.com>
2022-10-11 19:41:31 -0700

    Firestore testing

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,9 @@ import Media_Medium from './components/Media_Medium.vue'
 import Media_Small from './components/Media_Small.vue'
 
 import { initializeApp } from "firebase/app";
+import { getFirestore, collection, doc, getDocs, getDoc} from "firebase/firestore";
 import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyC5eQYZ60A4CNBG8h-FKQ9sYqRNW16Pnw0",
   authDomain: "media-manager-dc652.firebaseapp.com",
@@ -13,15 +14,24 @@ const firebaseConfig = {
   messagingSenderId: "801662514794",
   appId: "1:801662514794:web:68882ad471a6cbe3953c13"
 };
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const appCheck = initializeAppCheck(app, {
-  provider: new ReCaptchaV3Provider('6LeP-UEiAAAAACRYtJSFXP__7z2SvS_tcY5DWset'),
 
-  // Optional argument. If true, the SDK automatically refreshes App Check
-  // tokens as needed.
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider('6Le3h3MiAAAAAI-8QFZY76IDQwFsctEy5nW8zCGL'),
   isTokenAutoRefreshEnabled: true
 });
+
+const db = getFirestore(app);
+
+const docRef = doc(db, "Friend_Pages/Media");
+const docSnap = await getDoc(docRef);
+
+// if (docSnap.exists()) {
+//   console.log("Document data:", docSnap.data());
+// } else {
+//   // doc.data() will be undefined in this case
+//   console.log("No such document!");
+// }
 
 </script>
 

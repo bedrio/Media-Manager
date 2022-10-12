6756d99ec1257fc94c45e8ae62685d36ab611bbf
Bader Alrifai <bedrrifai1@gmail.com>
2022-10-11 20:42:58 -0700

    Modularized the Firestore code, and I also removed AppCheck

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <script setup>
+import { onMounted } from 'vue'
+
 import Media_Medium from './components/Media_Medium.vue'
 import Media_Small from './components/Media_Small.vue'
 
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, doc, getDocs, getDoc} from "firebase/firestore";
-import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
+import { getFirestore, collection, doc, getDocs, getDoc} from "firebase/firestore"
+import { db } from '@/Firestore.js'
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC5eQYZ60A4CNBG8h-FKQ9sYqRNW16Pnw0",
-  authDomain: "media-manager-dc652.firebaseapp.com",
-  projectId: "media-manager-dc652",
-  storageBucket: "media-manager-dc652.appspot.com",
-  messagingSenderId: "801662514794",
-  appId: "1:801662514794:web:68882ad471a6cbe3953c13"
-};
-const app = initializeApp(firebaseConfig);
-
-const appCheck = initializeAppCheck(app, {
-  provider: new ReCaptchaV3Provider('6Le3h3MiAAAAAI-8QFZY76IDQwFsctEy5nW8zCGL'),
-  isTokenAutoRefreshEnabled: true
-});
-
-const db = getFirestore(app);
-
-const docRef = doc(db, "Friend_Pages/Media");
-const docSnap = await getDoc(docRef);
+onMounted(async () => {
+  const docRef = doc(db, "Friend_Pages", "Media");
+  const docSnap = await getDoc(docRef);
+  console.log("docSnap", docSnap.data());
+})
 
 // if (docSnap.exists()) {
 //   console.log("Document data:", docSnap.data());
---
+++ src/Firestore.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC5eQYZ60A4CNBG8h-FKQ9sYqRNW16Pnw0",
+  authDomain: "media-manager-dc652.firebaseapp.com",
+  projectId: "media-manager-dc652",
+  storageBucket: "media-manager-dc652.appspot.com",
+  messagingSenderId: "801662514794",
+  appId: "1:801662514794:web:68882ad471a6cbe3953c13"
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export {
+    db
+}
